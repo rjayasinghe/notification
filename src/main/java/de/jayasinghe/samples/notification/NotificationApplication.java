c750dd8e5a395986089e166ff4e08498b0d8abb5
Robin de Silva Jayasinghe <r.de.silva.jayasinghe@sap.com>
2021-07-18 20:59:12 +0200

    enable http code
    also make the handlers async in order to support failures

--- src/main/java/de/jayasinghe/samples/notification/NotificationApplication.java
+++ src/main/java/de/jayasinghe/samples/notification/NotificationApplication.java
@@ -2,8 +2,10 @@ package de.jayasinghe.samples.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync(proxyTargetClass = true)
 public class NotificationApplication {
 
 	public static void main(String[] args) {
--- src/main/java/de/jayasinghe/samples/notification/out/NotificationSender.java
+++ src/main/java/de/jayasinghe/samples/notification/out/NotificationSender.java
@@ -6,6 +6,7 @@ import de.jayasinghe.samples.notification.events.NotificationShippedTriggered;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,28 +15,31 @@ import java.net.URI;
 @Component
 public class NotificationSender {
 	private static final Logger logger = LoggerFactory.getLogger(NotificationSender.class);
-	//private final RestTemplate restTemplate;
-	//private static final URI SMS_URI = URI.create("https://fancy-sms-service.net/send");
+	private final RestTemplate restTemplate;
+	private static final URI SMS_URI = URI.create("https://fancy-sms-service.net/send");
 
-	public NotificationSender(RestTemplate restTemplate) {
-		//this.restTemplate = restTemplate;
+	public NotificationSender() {
+		this.restTemplate = new RestTemplate();
 	}
 
 	@EventListener
+	@Async
 	public void on(NotificationPlacedTriggered notificationPlacedTriggered) {
-		//restTemplate.postForEntity(SMS_URI, notificationPlacedTriggered.getNotification(), String.class);
+		restTemplate.postForEntity(SMS_URI, notificationPlacedTriggered.getNotification(), String.class);
 		logger.info("sent orderPlaced notification {}", notificationPlacedTriggered.getNotification());
 	}
 
 	@EventListener
+	@Async
 	public void on(NotificationShippedTriggered notificationShippedTriggered) {
-		//restTemplate.postForEntity(SMS_URI, notificationShippedTriggered.getNotification(), String.class);
+		restTemplate.postForEntity(SMS_URI, notificationShippedTriggered.getNotification(), String.class);
 		logger.info("sent orderShipped notification {}", notificationShippedTriggered.getNotification());
 	}
 
 	@EventListener
+	@Async
 	public void on(NotificationDeliveredTriggered notificationDeliveredTriggered) {
-		//restTemplate.postForEntity(SMS_URI, notificationDeliveredTriggered.getNotification(), String.class);
+		restTemplate.postForEntity(SMS_URI, notificationDeliveredTriggered.getNotification(), String.class);
 		logger.info("sent orderDelivered notification {}", notificationDeliveredTriggered.getNotification());
 	}
 }
