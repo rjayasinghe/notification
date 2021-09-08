7813121400e69a0a84b84270beeb6a761bffcaac
Robin de Silva Jayasinghe <r.de.silva.jayasinghe@sap.com>
2021-09-08 21:40:25 +0200

    move initializer to dedicated class

--- src/test/java/de/jayasinghe/samples/notification/persistence/NotificationRepositoryTest.java
+++ src/test/java/de/jayasinghe/samples/notification/persistence/NotificationRepositoryTest.java
@@ -8,21 +8,19 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.support.TestPropertySourceUtils;
-import org.testcontainers.containers.GenericContainer;
+
+import de.jayasinghe.samples.notification.RedisContainerInitializer;
 
 @SpringBootTest
-@ContextConfiguration(initializers = NotificationRepositoryTest.Initializer.class)
-public class NotificationRepositoryTest {
+@ContextConfiguration(initializers = RedisContainerInitializer.class)
+class NotificationRepositoryTest {
 
 	@Autowired
 	private NotificationRepository repository;
 
 	@Test
-	void testThatItWorks() {
+	void testThatSavedEntityCanBeReadAgain() {
 		Notification notification = new Notification("orderId", "foo", "1234");
 		repository.save(notification);
 
@@ -30,23 +28,4 @@ public class NotificationRepositoryTest {
 		assertTrue(maybeFoundNotification.isPresent());
 		assertEquals("foo", maybeFoundNotification.get().getMessage());
 	}
-
-	public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-
-        private static final Integer PORT = 6379;
-
-		static GenericContainer<?> redisContainer = new GenericContainer<>("redis:6-alpine")
-            .withExposedPorts(PORT)
-            .withReuse(true);
-
-        @Override
-        public void initialize(ConfigurableApplicationContext context) {
-            // Start container
-            redisContainer.start();
-
-            String redisContainerIP = "spring.redis.host=" + redisContainer.getContainerIpAddress();
-            String redisContainerPort = "spring.redis.port=" + redisContainer.getMappedPort(PORT);
-            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(context,  redisContainerIP, redisContainerPort);
-        }
-    }
 }
