a4c127807a8d4f466c821837468802609a1fbde7
Robin de Silva Jayasinghe <r.de.silva.jayasinghe@sap.com>
2021-09-09 21:03:06 +0200

    add redis persistence for spring statemachine

--- src/main/java/de/jayasinghe/samples/notification/dispatch/statemachine/OrderStateMachineRuntimeConfig.java
+++ src/main/java/de/jayasinghe/samples/notification/dispatch/statemachine/OrderStateMachineRuntimeConfig.java
@@ -2,7 +2,13 @@ package de.jayasinghe.samples.notification.dispatch.statemachine;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.statemachine.StateMachineContextRepository;
+import org.springframework.statemachine.StateMachinePersist;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.data.redis.RedisStateMachineContextRepository;
+import org.springframework.statemachine.data.redis.RedisStateMachinePersister;
+import org.springframework.statemachine.persist.RepositoryStateMachinePersist;
 import org.springframework.statemachine.service.DefaultStateMachineService;
 import org.springframework.statemachine.service.StateMachineService;
 
@@ -11,9 +17,17 @@ public class OrderStateMachineRuntimeConfig {
 
 	@Bean
 	public StateMachineService<OrderStates, OrderEvents> stateMachineService(
-			StateMachineFactory<OrderStates, OrderEvents> stateMachineFactory) {
+			StateMachineFactory<OrderStates, OrderEvents> stateMachineFactory,
+			StateMachinePersist<OrderStates, OrderEvents, String> stateMachinePersist) {
 
-		return new DefaultStateMachineService<>(stateMachineFactory);
+		return new DefaultStateMachineService<>(stateMachineFactory, stateMachinePersist);
 	}
 
+	@Bean
+	public StateMachinePersist<OrderStates, OrderEvents, String> stateMachinePersist(
+			RedisConnectionFactory connectionFactory) {
+		RedisStateMachineContextRepository<OrderStates, OrderEvents> repository = new RedisStateMachineContextRepository<>(
+				connectionFactory);
+		return new RepositoryStateMachinePersist<>(repository);
+	}
 }
