c4304b44ccd772fa2df001c45f6f5aff6dc98c77
Robin de Silva Jayasinghe <r.de.silva.jayasinghe@sap.com>
2021-09-13 20:44:03 +0200

    let all notification events implement a single interface

--- src/main/java/de/jayasinghe/samples/notification/events/NotificationPlacedTriggered.java
+++ src/main/java/de/jayasinghe/samples/notification/events/NotificationPlacedTriggered.java
@@ -2,13 +2,14 @@ package de.jayasinghe.samples.notification.events;
 
 import de.jayasinghe.samples.notification.Notification;
 
-public class NotificationPlacedTriggered {
+public class NotificationPlacedTriggered implements NotificationEvent {
 	private final Notification notification;
 
 	public NotificationPlacedTriggered(Notification notification) {
 		this.notification = notification;
 	}
 
+	@Override
 	public Notification getNotification() {
 		return notification;
 	}
