51996b0f00ab889e057b5dfedc8ba7828ad996f0
Robin de Silva Jayasinghe <r.de.silva.jayasinghe@sap.com>
2021-10-04 15:03:53 +0200

    add test with external dependencies

---
+++ script.md
@@ -0,0 +1,18 @@
+* integration tests with external components
+** embedded implementations
+** abstractions are false friends
+** manually managed external compenents
+* integration tests with (docker) containers
+** maven plugin
+** manual docker compose
+* test containers
+** tool agnostic
+** pure java solution
+** ryuk container management
+* demo
+** add a container to a plain JUnit test
+** add a container to a Spring Boot test
+** wire container with ApplicationContextInitializer
+* outlook
+** JDBC Url Magic
+** 
---
+++ src/test/java/de/jayasinghe/samples/notification/persistence/redis/PlainNotificationRepositoryTest.java
@@ -0,0 +1,29 @@
+package de.jayasinghe.samples.notification.persistence.redis;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlainNotificationRepositoryTest {
+
+	@Autowired
+	private NotificationRepository repository;
+
+	@Test
+	@Disabled("does not work without manually started container")
+	void testThatSavedEntityCanBeReadAgain() {
+		Notification notification = new Notification("orderId", "foo", "1234");
+		repository.save(notification);
+
+		Optional<Notification> maybeFoundNotification = repository.findById("orderId");
+		assertTrue(maybeFoundNotification.isPresent());
+		assertEquals("foo", maybeFoundNotification.get().getMessage());
+	}	
+}
